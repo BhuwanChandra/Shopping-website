41cbcfa39f51dec10f0ef585fdc2002a832e7d57
BhuwanChandra <bhuwan7779@gmail.com>
2019-09-08 01:34:18 +0530

    added main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+$('.open-menu-icon').on('click',() => {
+    $('.main-nav-outer').addClass('open-menu');
+});
+
+
+$('.menu-close-icon').on('click',() => {
+    $('.main-nav-outer').removeClass('open-menu');
+});
+
